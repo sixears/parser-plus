5b740c33b708eb3f7c0cd45f7cf31dcde1b60b2f
Mr. Fluffy <git@sixearsm>
2022-11-18 18:08:33 +0000

    has-callstack->1.0.1.13

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
     more-unicode.url         = "github:sixears/more-unicode/r0.0.17.9";
     natural.url              = "github:sixears/natural/r0.0.1.10";
-    non-empty-containers.url = "github:sixears/non-empty-containers/r1.4.3.22";
+    non-empty-containers.url = github:sixears/non-empty-containers/r1.4.3.23;
     tasty-plus.url           = "github:sixears/tasty-plus/r1.5.2.16";
   };
 
